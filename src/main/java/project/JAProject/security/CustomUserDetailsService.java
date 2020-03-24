c9796297ef89d633869097361391c34bc6e3e945
DimaLishchyshyn <lishchyshyn1989@gmail.com>
2020-03-24 13:32:52 +0200

    added security layer

---
+++ src/main/java/project/JAProject/security/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package project.JAProject.security;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import project.JAProject.dao.UserRepository;
+import project.JAProject.dao.UserRolesRepository;
+import project.JAProject.domain.User;
+
+@Service("CustomUserDetailsService")
+public class CustomUserDetailsService implements UserDetailsService{
+
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private UserRolesRepository userRolesRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		
+		Optional<User> userOptional = userRepository.findByEmail(email);
+		
+		if(userOptional.isPresent()) {
+			List<String> userRoles = userRolesRepository.findRolesByUserName(email);
+			return new CustomUserDetails(userOptional.get(), userRoles);
+		}
+		
+		throw new UsernameNotFoundException("No user present with useremail:" + email);
+	}
+	
+}
